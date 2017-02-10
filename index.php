6cda32458486f9e4ce3aa926f596c8a9fe9ee391
BenKemp96 <99031541@mydavinci.nl>
2017-02-10 11:45:13 +0100

    canvas fullscreen

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Project Blaze</title>
-	<link rel="stylesheet" href="">
-	<script type="text/javascript" src="main.js"></script>
-	<script type="text/javascript" src="p5.min.js"></script>
+	<title>Project Komodo</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<canvas id="main" width="600" height="400" style="border:3px solid red;"></canvas>
+	<canvas id="main"></canvas>
+
+	<footer>
+		<script type="text/javascript" src="main.js"></script>
+		<!-- <script type="text/javascript" src="p5.min.js"></script> -->
+	</footer>
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-var c = document.getElementById("main");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
-ctx.stroke();
+var canvas = document.getElementById('main');
+		canvasCtx = canvas.getContext('2d');
+
+function fullscreen(){
+	if (canvas.webkitRequestFullScreen) {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		canvas.webkitRequestFullScreen();
+	}
+	else {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		canvas.mozRequestFullScreen();
+	}            
+}
+
+canvasCtx.fillstyle = "red";
+canvasCtx.fill();
+
+canvas.addEventListener("click",fullscreen);
