f764e3129f4dd958ac4568b31e93489f279ee931
BenKemp96 <99031541@mydavinci.nl>
2017-02-21 14:12:48 +0100

    versionless

--- index.php
+++ index.php
@@ -5,13 +5,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Project Komodo</title>
 	<link rel="stylesheet" href="style.css">
+	<script type="text/javascript" src="main.js"></script>
+	<!-- <script type="text/javascript" src="p5.min.js"></script> -->
 </head>
 <body>
 	<canvas id="main"></canvas>
 	<button id="start" type="button">start</button>
-	<footer>
-		<script type="text/javascript" src="main.js"></script>
-		<!-- <script type="text/javascript" src="p5.min.js"></script> -->
-	</footer>
 </body>
 </html>
