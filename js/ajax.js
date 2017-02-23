7354e4bf47423ad08bf13478bfaeb5d1a44cdf8e
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2017-02-23 09:00:44 +0100

    Alpha 0.03
    made a database connection

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,12 @@
-document.onload = function() {
+window.onload = function() {
+    console.log('1');
     document.getElementById('startGet').onclick = function() {
+        console.log('2');
         var getMonsters = new XMLHttpRequest();
-        getMonsters.open(); 
+        console.log('3');
+        getMonsters.open('GET', './php/ajax.php?table=bestiary', false);
+        console.log('4');
+        getMonsters.send();
+        console.log(getMonsters.responseText);
     }
 }
