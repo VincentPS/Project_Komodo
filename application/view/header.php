25e52ae92e5855e6fe75719a977336acf4593ae3
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2017-03-09 11:24:43 +0100

    Alpha 0.11
    made the header for the html

---
+++ application/view/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" >
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+	<link rel="stylesheet" href="style/style.css" >
+	<script type="text/javascript" src="js/main.js" ></script>
+	<title>Project Komodo</title>
+</head>
+<body>
