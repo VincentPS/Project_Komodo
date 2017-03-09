6df68a82d6b8f188197be9872bf89cfa4f5f05f8
BenKemp96 <99031541@mydavinci.nl>
2017-03-09 13:03:49 +0100

    Alpha 0.11.2

--- application/view/index.php
+++ application/view/index.php
@@ -1,4 +1,11 @@
-	<section>
-		<canvas id="main" ></canvas>
-	</section>
-	<button id="start" type="button" >start</button>
+<section class="canvas">
+	<canvas id="main" width="960" height="640">Your browser may not support the HTML5 canvas element. Consider upgrading to a browser which does, such as: Google Chrome or Mozilla Firefox.</canvas>
+</section>
+<section class="description">
+	<button id="start" type="button">start</button>
+	<button id="startGet">Click to get database</button>
+	<h2>We know it's a long explaination, Still advised to read it.</h2>
+	<p>
+		This game is meant to be played in fullscreen mode. The moment you hit the start button, you will automatically be put into fullscreen mode for this reason. Press the escape key to cancel out of the canvas and back to the standard page.
+	</p>
+</section>
