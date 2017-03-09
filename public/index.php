185bec35636dd1eea3a7de99a89efb594066097d
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2017-03-09 11:10:16 +0100

    Alpha 0.10
    Major change in the structural layout of the application

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    // defines the ROOT and APP variable
+    define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+    define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+
+    // includes the files
+    require APP . 'config/config.php';
+    require APP . 'core/application.php';
+
+    // makes the application
+    $App = new Application();
---
+++ public/js/ajax.js
@@ -0,0 +1,11 @@
+// // when the window is loaded activaten the folowing
+// window.onload = function() {
+//
+//     // when the button is clicked get the bestiary table data
+//     document.getElementById('startGet').onclick = function() {
+//         var data = new XMLHttpRequest();
+//         data.open('POST', './php/ajax.php', false);
+//         data.send();
+//         document.getElementById('displayData').innerHTML = data.responseText;
+//     }
+// }
