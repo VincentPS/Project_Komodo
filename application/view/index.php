35203d79eb69b4a8ddd4d20ad4f318df87764649
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2017-03-09 11:10:16 +0100

    Alpha 0.10
    Major change in the structural layout of the application

--- application/view/index.php
+++ application/view/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <section class="canvas">
 	<canvas id="main" width="960" height="640">Your browser may not support the HTML5 canvas element. Consider upgrading to a browser which does, such as: Google Chrome or Mozilla Firefox.</canvas>
 </section>
@@ -9,3 +10,26 @@
 		This game is meant to be played in fullscreen mode. The moment you hit the start button, you will automatically be put into fullscreen mode for this reason. Press the escape key to cancel out of the canvas and back to the standard page.
 	</p>
 </section>
+=======
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" >
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+	<link rel="stylesheet" href="style/style.css" >
+	<script type="text/javascript" src="js/main.js" ></script>
+	<title>Project Komodo</title>
+</head>
+<body>
+	<section>
+		<canvas id="main" ></canvas>
+	</section>
+	<button id="start" type="button" >start</button>
+	<p id="displayData" ></p>
+	<script>
+		window.data = JSON.parse('<?php echo json_encode($application->data) ?>');
+		console.log(window.data);
+	</script>
+</body>
+</html>
+>>>>>>> Alpha 0.10
--- public/index.php
+++ public/index.php
@@ -8,6 +8,11 @@
     require APP . 'config/config.php';
     require APP . 'core/application.php';
 
+<<<<<<< HEAD
     // makes the game
     $game = new Application();
     $game->render();
+=======
+    // makes the application
+    $App = new Application();
+>>>>>>> Alpha 0.10
