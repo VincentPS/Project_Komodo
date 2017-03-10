49f52fbce89c6af9b546c69af7b1f1032bfa11b5
WesselvanZeggeren <wessel.van.zeggeren@gmail.com>
2017-03-10 16:04:14 +0100

    Alpha 0.20.0
    Changed the layout. Now has a login and a register functionality. The
    old verson is completely put in the folder _oldKomodo (if you forget how
    to old stucture worked). Certain files like script.js are already placed
    in the js folder of the new layout just a images and styling.

--- public/index.php
+++ public/index.php
@@ -1,13 +1,22 @@
 <?php
 
-    // defines the ROOT and APP variable
-    define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-    define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+    // checking for minimum PHP version
+    if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+        exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+    } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
+        require_once("libraries/password_compatibility_library.php");
+    }
 
-    // includes the files
-    require APP . 'config/config.php';
-    require APP . 'core/application.php';
+    // includes the php files
+    require_once("../application/config/config.php");
+    require_once("../application/core/application.php");
 
-    // makes the game
-    $game = new Application();
-    $game->render();
+    // creates the application object
+    $APP = new Application();
+
+    // load the game or the login page
+    if ($APP->isUserLoggedIn() == true) {
+        include("../application/view/hero.php");
+    } else {
+        include("../application/view/login.php");
+    }
